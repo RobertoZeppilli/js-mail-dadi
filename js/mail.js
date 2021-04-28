3dd39619df0753b4670e175d80d6c5fe2a631b7a
RobertoZeppilli <robzep171@gmail.com>
2021-04-28 16:28:58 +0200

    Mail list checking

---
+++ js/mail.js
@@ -0,0 +1,26 @@
+// 1. Email presenti nella lista
+var mails = ["gianni@gmail.com", "andrea@gmail.com", "marco@gmail.com", "anna@gmail.com", "francesca@gmail.com",]
+
+// 2. richiesta della mail all'utente
+var mailUtente = prompt("Qual'è la tua email?");
+document.getElementById("mail").innerHTML = mailUtente;
+
+// 2.1
+var check = false;
+
+// 3. Ciclo per confrontare se la mail utente è presente nella lista
+for (var i = 0; i < mails.length; i++) {
+
+    if (mailUtente == mails[i]) {
+        check = true;
+    }
+}
+
+// 4. Stampa messaggio (fuori dal ciclo) nel caso in cui la mail sia presente o meno
+if (check == true) {
+    document.getElementById("conferma").innerHTML = "Complimenti sei nella lista!";
+} else {
+    document.getElementById("conferma").innerHTML = "Spiacente, non sei in lista!";
+}
+
+
